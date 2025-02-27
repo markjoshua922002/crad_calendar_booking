a0080574bd7469d8db10de5078b144103f1a5f9d
markjoshua922002 <markjoshuatrinidad02@gmail.com>
2025-02-28 01:42:59 +0800

    move width

--- index.php
+++ index.php
@@ -129,7 +129,7 @@ while ($row = $bookings->fetch_assoc()) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Smart Scheduling System</title>
-    <link rel="stylesheet" href="mycss/style.css?v=2">
+    <link rel="stylesheet" href="mycss/style.css?v=3">
     <link rel="stylesheet" href="mycss/sidebar.css?v=1">
     <link rel="stylesheet" href="mycss/calendar.css?v=8">
     <link rel="stylesheet" href="mycss/day.css">
