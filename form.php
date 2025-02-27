4343fd636aaf582b110991615bf8cccdfe06fd73
markjoshua922002 <markjoshuatrinidad02@gmail.com>
2025-02-28 02:15:19 +0800

    form

---
+++ form.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form - BCP CRAD</title>
+    <link rel="stylesheet" href="mycss/style.css?v=3">
+    <link rel="stylesheet" href="mycss/sidebar.css?v=1">
+</head>
+<body>
+<button class="menu-button" id="menuButton">&#9776;</button> <!-- Menu button -->
+
+<div class="sidebar" id="sidebar">
+    <a href="index.php">CRAD</a>
+    <a href="osas.php">OSAS</a>
+    <div style="flex-grow: 1;"></div> <!-- Spacer to push logout button to the bottom -->
+    <a href="logout.php" class="logout-button">Logout</a>
+</div>
+
+<div class="container">
+    <!-- Add your content here -->
+</div>
+
+<script defer src="js/script.js"></script>
+</body>
+</html>
