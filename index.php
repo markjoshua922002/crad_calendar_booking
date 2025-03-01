9ac518a6b3ec1b864330302c6a73e683d99e6ad9
markjoshua922002 <markjoshuatrinidad02@gmail.com>
2025-03-01 08:55:42 +0800

    appointment modal width scroll bar

--- index.php
+++ index.php
@@ -135,9 +135,9 @@ while ($row = $bookings->fetch_assoc()) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Smart Scheduling System</title>
-    <link rel="stylesheet" href="mycss/style.css?v=6">
+    <link rel="stylesheet" href="mycss/style.css?v=7">
     <link rel="stylesheet" href="mycss/sidebar.css?v=1">
-    <link rel="stylesheet" href="mycss/calendar.css?v=18">
+    <link rel="stylesheet" href="mycss/calendar.css?v=19">
     <link rel="stylesheet" href="mycss/day.css">
     <link rel="stylesheet" href="mycss/reminder.css?v=11">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-timepicker/1.13.18/jquery.timepicker.min.css">
