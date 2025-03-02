74107076143a5a8404367b553b377c044c02e677
markjoshua922002 <markjoshuatrinidad02@gmail.com>
2025-03-02 11:30:00 +0800

    go back again

--- index.php
+++ index.php
@@ -183,8 +183,8 @@ while ($row = $bookings->fetch_assoc()) {
     <a href="index.php">CRAD</a>
     <a href="osas.php">OSAS</a>
     <a href="form.php">LOGBOOK</a>
-    <a href="accounts.php">Users</a>
     <div style="flex-grow: 1;"></div> <!-- Spacer to push logout button to the bottom -->
+    <a href="accounts.php">Users</a>
     <a href="logout.php" class="logout-button">Logout</a>
 </div>
 <div class="container">
