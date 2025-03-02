5d393994bc41d2a0a47f43615ef1e80e1433f2c5
markjoshua922002 <markjoshuatrinidad02@gmail.com>
2025-03-02 08:18:53 +0800

    register css

--- register.php
+++ register.php
@@ -35,7 +35,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['register'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
-    <link rel="stylesheet" href="css/style_login.css">
+    <link rel="stylesheet" href="mycss/style_login.css">
 </head>
 <body>
     <div class="login-container">
