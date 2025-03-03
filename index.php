f65d6893da1f20fe6c573216cba4cde2fc143cf9
markjoshua922002 <markjoshuatrinidad02@gmail.com>
2025-03-04 01:08:54 +0800

    update 3

--- index.php
+++ index.php
@@ -154,11 +154,8 @@ while ($row = $bookings->fetch_assoc()) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Smart Scheduling System</title>
-    <link rel="stylesheet" href="mycss/style.css?v=14">
-    <link rel="stylesheet" href="mycss/sidebar.css?v=3">
-    <link rel="stylesheet" href="mycss/calendar.css?v=26">
-    <link rel="stylesheet" href="mycss/day.css">
-    <link rel="stylesheet" href="mycss/reminder.css?v=11">
+    <link rel="stylesheet" href="mycss/style.css?v=15">
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-timepicker/1.13.18/jquery.timepicker.min.css">
     <link rel="icon" href="assets/bcplogo.png" type="image/png">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
